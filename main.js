37dcb3b61aafd544d5f1e7e561003763e22cdcad
Elias Weiss <elias.weiss8@icloud.com>
2023-03-22 21:03:15 +0100

    added form activation, marup and styling

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert('Not able to access, please fill out the form.');
+// alert('Verifizierung fehlgeschlagen, bitte erneut ausfüllen.');
+function goGo() {
+    const contentInputA = 1;
+    const contentInputB = 1;
+    if(contentInputA != false && contentInputB != false) {
+        console.log('test')
+    }
+    document.getElementsByTagName('body')[0].classList.toggle('activated');
+}
+document.getElementById('send').addEventListener('click', goGo);
