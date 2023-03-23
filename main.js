df6dc4c6d55fbee2a3375de44c44ba976dda91b3
Elias Weiss <elias.weiss8@icloud.com>
2023-03-23 20:36:46 +0100

    add condition something in field

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 alert('Verifizierung fehlgeschlagen, bitte erneut ausfüllen.');
 function goGo() {
-    const contentInputA = 1;
-    const contentInputB = 1;
-    if(contentInputA != false && contentInputB != false) {
-        console.log('test')
+    const contentInputA = document.getElementsByTagName('input')[0].value;
+    const contentInputB = document.getElementsByTagName('input')[1].value;
+    console.log(contentInputA, contentInputB)
+    if(contentInputA !== "" && contentInputB !== "") {
+        document.getElementsByTagName('body')[0].classList.toggle('activated');
+    } else {
+        alert('Bitte Felder ausfüllen.')
     }
-    document.getElementsByTagName('body')[0].classList.toggle('activated');
 }
 document.getElementById('send').addEventListener('click', goGo);
